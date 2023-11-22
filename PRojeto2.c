20dd0690d37be0e760f98e64812ed4eb7366bd73
Luerfel <97566776+Luerfel@users.noreply.github.com>
2023-11-22 14:02:49 -0300

    Update PRojeto2.c

--- PRojeto2.c
+++ PRojeto2.c
@@ -2,70 +2,214 @@
 #include <stdio.h>
 #include <time.h>
 #include <locale.h> //necessário para usar setlocale
+#include <math.h>
+#include <ctype.h>
+
+// estrutura do vetor
 typedef struct No{
     int chave; // chave para ordenação do vetor
     float valor; // valor do nó 
 } no;
 
-// funções 
+// Protótipos das funções 
 void criarVetorAleatorio(no *vetor, int size);
-void chaveAleatorio(no *vetor , int size);
+void criarVetorOrdenado(no *vetor, int size);
 void imprimevetor(no *vetor, int tamanho);
-void menu();
-void menuTamanho();
-no insertionSort(no *vetor, int tamanho);
+void insertionSort(no *vetor, int tamanho,int c);
 
+// protótipos de funções auxiliares
+void limparTela();
+void limparBuffer();
+void imprimevetor(no *vetor, int tamanho);
+int lerOpcao(int tamanho);
+void pausaEnter();
+char validarOpcao();
+void mensagemFinal();
+void menu();
+void voltaMenu();
+void escolhaOrdenacao();
+void escolhaMetodo();
+void escolhaContinuar();
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
     // declaração dos vetores
-
-    no vetor10Mil[10000]; // vetor 10⁴
-    no vetor50Mil[50000]; // vetor 5*10⁴
-    no vetor100Mil[100000]; // vetor 10⁵
-    no vetor500Mil[500000];  // vetor 5*10⁵
-    no vetor1Milhao [1000000];  // vetor 10*6
-
-    // declaração das variaveis
-
     char tipo = 'a';
     clock_t inicio, fim;
     double tempo_execucao;
-    int escolha;
+    int escolha,tamanho,escolha2,escolha3,escolha4;
+    int c = 0;
+
+  do {
+        menu();
+        escolha = lerOpcao(6);
+        switch (escolha) {
+            case 1:
+                tamanho = 10000;
+                no *vetor= (no *)malloc(tamanho * sizeof(int));
+                limparTela();
+                void escolhaOrdenacao();
+                escolhaOrdenacao();
+                escolha2 = lerOpcao(2);
+                limparTela();
+                if (escolha2 == 1){
+                    criarVetorOrdenado(vetor,tamanho);
+                    escolhaMetodo();
+                    escolha3 = lerOpcao(6);
+                    limparTela();
+                    switch(escolha3){
+
+                        case 1:
+                            do {
+                                inicio = clock();
+                                insertionSort(vetor,tamanho,c);
+                                fim  = clock();
+                                tempo_execucao = ((double)(fim - inicio)) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
+                                printf("Deseja imprimir o vetor?\n1. Sim\n2. Não\n");
+                                escolha = lerOpcao(2);
+                                if (escolha==1){
+                                    imprimevetor(vetor,tamanho);
+                                }
+
+                                printf("Tempo de execução em segundos : %f\n\n",tempo_execucao);
+                                printf("Seed : %d\n",c+22011027);
+                                free(vetor);                                                 // libera memoria do vetor
+                                pausaEnter();
+                                limparTela();
+                                escolhaContinuar();
+                                escolha4 = lerOpcao(2);
+                                if (escolha4 == 1)
+                                c++;                                                         // usada para mudar a seed
+                                else if(escolha4 != 1){
+                                    voltaMenu();
+                                    escolha4 = 1;
+                                }
+                            }while(escolha4==1);                                               
+                        break;
+
+                        case 2:
+
+                        break;
+
+                        case 3:
+
+                        break;
+                        case 4:
+
+                        break;
+                        
+                        case 5:
+
+                        break;
+
+                        case 6:
+                        
+                        break;
+
+                        default:
+                        limparTela();
+                        printf("Opção INVALIDA!\n\n");
+                        pausaEnter();
+                        limparTela();
+                        free(vetor);
+                        voltaMenu();
+                        break;
+                    }
+
+                }
+                else if(escolha2==2){
+                    criarVetorAleatorio(vetor,tamanho);
+                    escolhaMetodo();
+                    escolha3 = lerOpcao(6);
+                    limparTela();
+                    switch(escolha3){
+
+                        case 1:
+
+                        break;
+
+                        case 2:
+
+                        break;
+
+                        case 3:
+
+                        break;
+                        case 4:
+
+                        break;
+                        
+                        case 5:
+
+                        break;
+
+                        case 6:
+                        
+                        break;
+
+                        default:
+                        limparTela();
+                        printf("Opção INVALIDA!\n\n");
+                        pausaEnter();
+                        limparTela();
+                        free(vetor);
+                        voltaMenu();
+                        break;
+                    }
+                }
+                else{
+                    limparTela();
+                    printf("Opção invalida!\n");
+                    free(vetor);
+                    voltaMenu();
+                    limparTela();
+                }
+                break;
+            case 2:
+                printf("Você escolheu a opção 2\n");
+                break;
+            case 3:
+                printf("Você escolheu a opção 3\n");
+                break;
+            case 4:
+                printf("Saindo do programa\n");
+                break;
+            case 5:
+                printf("Saindo do programa\n");
+                break;
+            case 6:
+                limparTela();
+                mensagemFinal();
+                break;
+            default:
+                limparTela();
+                printf("Opção inválida\n");
+                pausaEnter();
+                limparTela();
+                voltaMenu();
+        
+                break;
+        }
+    } while (escolha != 6);
 
 
-    /* VETOR DE 10 MIL 
-    criarVetorAleatorio(vetor10Mil, 10000);
-    inicio = clock(); // Marca o início da execução
 
-    insertionSort(vetor10Mil,10000);
 
-    fim = clock(); // Marca o fim da execução
-    imprimevetor(vetor10Mil, 10000);
-    tempo_execucao = ((double)(fim - inicio)) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
-   */
+  
+    printf("fim do programa");
 
-    criarVetorAleatorio(vetor50Mil,50000);
-    inicio = clock(); // Marca o início da execução
-
-    insertionSort(vetor50Mil,50000);
-    fim = clock(); // Marca o fim da execução
-    tempo_execucao = ((double)(fim - inicio)) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
-    printf("Tempo de execução: %f segundos\n", tempo_execucao);
+//     tempo_execucao = ((double)(fim - inicio)) / CLOCKS_PER_SEC; // Calcula o tempo decorrido em segundos
 
 
 
 
-
-
-    //printf("Tempo de execução: %f segundos\n", tempo_execucao);
 
 
 
     }
    
 
-
+ // Funções de ordenação e criação vetor
 
 void criarVetorAleatorio(no *vetor, int size) {
     int i;
@@ -74,13 +218,48 @@ void criarVetorAleatorio(no *vetor, int size) {
     for(i = 0; i < size; i++) 
     {
         
-        vetor[i].chave = i+1;
+        vetor[i].chave = (int)(rand()+100); // chave aleatoria
         
         vetor[i].valor = (float)(rand() + 100); // gera um número aleatório acima de 100 e armazena no valor
        
     }
 
 }
+
+void criarVetorOrdenado(no *vetor, int size) {
+    int i;
+    srand(22011027); // Semente
+
+    for(i = 0; i < size; i++) 
+    {
+        
+        vetor[i].chave = 88+3*i*i;
+        vetor[i].valor = (float)(rand() + 100); // gera um número aleatório acima de 100 e armazena no valor
+       
+    }
+
+}
+
+void insertionSort(no *vetor, int tamanho,int c){
+    int i, j,escolha;
+    srand(22011027+i);
+    no aux;
+    for (i = 1; i<tamanho; i++){
+       for(j = i; j>0; j--){
+            if (vetor[j-1].chave<vetor[j].chave){
+            aux = vetor[j-1];
+            vetor[j-1] = vetor[j];
+            vetor [j] = aux;        } 
+       }
+    }
+
+
+
+}
+
+
+
+// Funções auxiliares
 void imprimevetor(no *vetor, int tamanho){
     int i;
     for(i = 0; i < tamanho; i++)
@@ -89,25 +268,123 @@ void imprimevetor(no *vetor, int tamanho){
         printf("Chave: %d, Valor: %.2f\n", vetor[i].chave, vetor[i].valor); // imprime a chave e o valor
     }
 }
-void chaveAleatorio(no *vetor , int size){
-    int i;
-    for (i = size - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
-        int temp = vetor[i].chave;
-        vetor[i].chave = vetor[j].chave;
-        vetor[j].chave = temp;
+
+void menu(){
+    printf("Digite o tamanho do seu vetor:\n");
+    printf("1. 10⁴ (10 MIL)\n");
+    printf("2. 5*10⁴ (50 MIL)\n");
+    printf("3. 10⁵ (100 MIL)\n");
+    printf("4. 5*10⁵ (500 mil)\n");
+    printf("5. 5⁶ (1 Milhão)\n");
+    printf("6. Finalizar Programa\n\n");
+}
+
+void voltaMenu()
+{
+
+    char opcao;
+    printf("Deseja voltar para o menu principal? S/N\n");
+    opcao = validarOpcao();
+
+    if (tolower(opcao) == 's')
+    {
+        limparTela();
+        return;
+    }
+    else if (tolower(opcao == 'n'))
+    {
+        limparTela();
+        mensagemFinal();
+        exit(0);
     }
 }
-no insertionSort(no *vetor, int tamanho){
-    int i, j;
-    no aux;
-    for (i = 1; i<tamanho; i++){
-       for(j = i; j>0; j--){
-            if (vetor[j-1].chave<vetor[j].chave){
-            aux = vetor[j-1];
-            printf("%d\n",i);
-            vetor[j-1] = vetor[j];
-            vetor [j] = aux;        } 
-       }
-    }
+
+void limparTela()
+{
+    // essa função assim como todo os programas foi pensando para que ele funciona em qualquer sistema operacional
+#if defined(_WIN32)
+    system("cls");
+#elif defined(__linux__)
+    system("clear");
+#elif defined(__APPLE__)
+    system("clear");
+#endif
 }
+
+void limparBuffer()
+{
+    int c;
+    do
+    {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+int lerOpcao(int tamanho) {
+    int codigo;
+  
+    do {
+        printf("Digite a opção desejada : ");
+
+    if(scanf("%d", &codigo) == 1) {
+        limparBuffer();
+        return codigo;
+    }
+    else if (codigo >= 1 && codigo <=tamanho)
+    {
+        limparBuffer();
+        limparTela();
+        return 99;
+    }
+    }while(1); }
+
+void pausaEnter() {
+
+    printf("Pressione ENTER para continuar...\n");
+
+    int ch = getchar();
+    while(ch != '\n' && ch != EOF) {
+        ch = getchar();
+  }
+
+}
+
+char validarOpcao() {
+
+    char opcao;
+
+    do {
+        opcao = tolower(getchar());
+        limparBuffer(); 
+    if(opcao != 's' && opcao != 'n') {
+        printf("Opção inválida. Digite apenas s ou n.\n");
+    }
+
+  } while(opcao != 's' && opcao != 'n');
+    return opcao;
+
+}
+
+void mensagemFinal(){
+    printf("\nFIM DO PROGRAMA!\n\n");
+    printf("Obrigado por usar nosso software. Ficamos felizes em ajudar.\nAté a próxima vez!\n\n");
+}
+
+void escolhaOrdenacao(){
+    printf("Como você prefere que a chave de ordenação seja? \n");
+    printf("1. Ordenada\n");
+    printf("2. Aleatoria\n");
+}
+
+void escolhaMetodo(){
+    printf("Qual o metodo de ordenação?\n");
+    printf("1. InsertionSort\n");
+    printf("2. BubleSort\n");
+    printf("3. ShellSort\n");
+    printf("4. MergeSort\n");   
+    printf("5. QuickSort\n");
+    printf("6. ShellSort\n");
+
+}
+
+void escolhaContinuar(){
